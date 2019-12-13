6190fe5415b387d2cc14a03cfba889cf9578df91
Logeshwari Murugan <logeshwari.murugan@innoart.local>
2019-12-13 16:14:28 +0530

    iphone change

--- src/app/pages/public/verify/verify.page.ts
+++ src/app/pages/public/verify/verify.page.ts
@@ -26,10 +26,10 @@ export class verifyPage implements OnInit {
     if(form.form.invalid){
       return;
     }
-    else if(!this.loginObj['digit1'] || !this.loginObj['digit2']|| !this.loginObj['digit3'] || !this.loginObj['digit4']
-      || !this.loginObj['digit5'] || !this.loginObj['digit6']){
-        this.invalidOTP = true;
-    }
+    // else if(!this.loginObj['digit1'] || !this.loginObj['digit2']|| !this.loginObj['digit3'] || !this.loginObj['digit4']
+    //   || !this.loginObj['digit5'] || !this.loginObj['digit6']){
+    //     this.invalidOTP = true;
+    // }
     else{
       this.router.navigate(["register"]);
     }
@@ -37,34 +37,29 @@ export class verifyPage implements OnInit {
 
   click(){
     this.invalidOTP = false ;
-    $('.digit-group').find('input').each(function() {
-    $(this).attr('maxlength', 1);
-    $(this).on('keyup', function(e) {
-      console.log("HIT????")
-      // if(!isNaN(e.originalEvent.key))
-      // {
-      var parent = $($(this).parent());
-      
-      if(e.keyCode === 8 || e.keyCode === 37) {
-        var prev = parent.find('input#' + $(this).data('previous'));
-        
-        if(prev.length) {
-          $(prev).select();
+    var charLimit = 1;
+    $(".inputs").keydown(function(e) {
+        var keys = [8, 9, /*16, 17, 18,*/ 19, 20, 27, 33, 34, 35, 36, 37, 38, 39, 40, 45, 46, 144, 145];
+        console.log("e.which")
+        console.log(e.which)
+        console.log("this.value.substr(-1).charCodeAt(0)")
+        console.log(this.value.substr(-1).charCodeAt(0))
+        if (e.which == 8 && this.value.length == 0) {
+            $(this).prev('.inputs').focus();
+        } else if ($.inArray(e.which, keys) >= 0) {
+            return true;
+        } else if (this.value.length >= charLimit) {
+            $(this).next('.inputs').focus();
+            return false;
+        } else if (e.shiftKey || e.which <= 48 || e.which >= 58) {
+            return false;
         }
-      } else if((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 65 && e.keyCode <= 90) || (e.keyCode >= 96 && e.keyCode <= 105) || e.keyCode === 39) {
-        var next = parent.find('input#' + $(this).data('next'));
-        
-        if(next.length) {
-          $(next).select();
-        } else {
-          if(parent.data('autosubmit')) {
-            parent.submit();
-          }
-        // }
+    }).keyup (function () {
+        if (this.value.length >= charLimit) {
+            $(this).next('.inputs').focus();
+            return false;
         }
-      }
     });
-  });
   }
 
 
