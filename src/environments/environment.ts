1a7f0e7d2071cb1a6b04ac144b82c516080b4b5f
arunaraja <31388833+arunaraja@users.noreply.github.com>
2019-12-23 17:00:22 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   /*baseUrl: 'http://192.168.1.6:8090'*/
-  baseUrl: 'https://emapi.innoart.in:3000'
+  baseUrl: 'https://emapi.innoart.in'
 };
 
 /*
